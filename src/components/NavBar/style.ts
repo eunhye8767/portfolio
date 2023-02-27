94b528068fb672ff34c749e9fecb01d26562700b
leh87 <ehlee@brainednet.com>
2023-02-27 15:16:07 +0900

    navbar css 수정

--- src/components/NavBar/style.ts
+++ src/components/NavBar/style.ts
@@ -1,24 +1,45 @@
 import styled from "styled-components";
-import { flexAlignCen, flexCenter, flexColumn, flexJustCen, FontStyle, SizeWH } from "Assets/MixinStyle";
+import {
+  flexCenter,
+  flexColumn,
+  flexJustCen,
+  FontStyle,
+  SizeWH,
+} from "Assets/MixinStyle";
 import { width, color } from "Assets/StyleVariable";
 
 export const NavSection = styled.nav`
   ${flexCenter}
-  ${SizeWH("100%", "80px")}
 
   position: fixed;
   bottom: 20px;
-  left: 0;
   z-index: 900;
   pointer-events: none;
+
+  @media (min-width: ${width.minMobile}) {
+    ${SizeWH("100%", "80px")}
+    left: 0;
+  }
+
+  @media (max-width: ${width.mobile}) {
+    ${SizeWH("calc(100% - 40px)", "60px")}
+    left: 20px;
+  }
 `;
 
 export const NavArea = styled.div`
   ${SizeWH(width.minMobile, "100%")}
-  
+
   background: ${color.blue};
-  border-radius: 20px;
-  `;
+
+  @media (min-width: ${width.minMobile}) {
+    border-radius: 20px;
+  }
+
+  @media (max-width: ${width.mobile}) {
+    border-radius: 10px;
+  }
+`;
 
 export const NavGroup = styled.ul`
   ${flexCenter}
@@ -35,27 +56,56 @@ export const NavGroup = styled.ul`
       opacity: 1;
     }
   }
+
+  @media (max-width: ${width.mobile}) {
+    justify-content: space-between;
+    padding: 0 20px;
+  }
+
+  @media (max-width: 359px) {
+    padding: 0 10px;
+  }
 `;
 
 export const NavItem = styled.button`
   ${flexColumn}
-
-  padding-bottom: 24px;
   pointer-events: all;
 
   .ico {
     ${flexCenter}
-    ${SizeWH("24px")}
   }
 
   .txt {
-    ${FontStyle(12, 700, color.white, "14px")}
-
     position: absolute;
     bottom: 0;
     left: 50%;
-    margin-top: 10px;
     transform: translateX(-50%);
     text-transform: uppercase;
+  }
+
+  @media (min-width: ${width.minMobile}) {
+    padding-bottom: 24px;
+
+    .ico {
+      ${SizeWH("24px")}
+    }
+
+    .txt {
+      ${FontStyle(12, 700, color.white, "14px")}
+      margin-top: 10px;
+    }
+  }
+
+  @media (max-width: ${width.mobile}) {
+    padding-bottom: 20px;
+
+    .ico {
+      ${SizeWH("20px")}
+    }
+
+    .txt {
+      ${FontStyle(10, 700, color.white, "12px")}
+      margin-top: 8px;
+    }
   }
 `;
