db4b88af46206ccae953f276383af8a099d0d036
leh87 <ehlee@brainednet.com>
2023-03-21 11:50:14 +0900

    skills 타입 수정

--- src/db/DataSkills.ts
+++ src/db/DataSkills.ts
@@ -1,209 +1,209 @@
 const DataSkills = {
-  mainTitle: ["재밌게","배워요"],
+  mainTitle: ["재밌게", "배워요"],
   subTitle: "기능구현",
   slide: [
     {
       id: 1,
       title: "블루레이 추진<br />호텔델루나 팬페이지",
-      img:"/db/skills/s_slide_1.png",
+      img: "/db/skills/s_slide_1.png",
       type: ["반응형"],
       desc: "블루레이 추친을 위한 팬페이지",
       url: "https://leh317.iwinv.net/skills/slide/1/delluna",
-      ia: ""
+      ia: "",
     },
     {
       id: 2,
       title: "비플레인(beplain)<br />라쿠텐 모바일 페이지",
-      img:"/db/skills/s_slide_2.png",
+      img: "/db/skills/s_slide_2.png",
       type: ["적응형", "MO"],
       desc: "인터렉티브 웹 만들기 클론코딩",
       url: "https://leh317.iwinv.net/skills/slide/2/beplain",
-      ia: ""
+      ia: "",
     },
     {
       id: 3,
       title: "인프런 1분코딩<br />클론코딩, 전진! 3D 스크롤",
-      img:"/db/skills/s_slide_3.png",
-      type: ["반응형"],
+      img: "/db/skills/s_slide_3.png",
+      type: ["적응형", "PC"],
       desc: "스크롤 이벤트 구현",
       url: "https://leh317.iwinv.net/skills/slide/3",
-      ia: ""
+      ia: "",
     },
     {
       id: 4,
       title: "인프런 1분코딩<br />클론코딩, BBC 페이지",
-      img:"/db/skills/s_slide_4.png",
-      type: ["반응형"],
+      img: "/db/skills/s_slide_4.png",
+      type: ["적응형", "PC"],
       desc: "스크롤 이벤트 구현",
       url: "https://leh317.iwinv.net/skills/slide/4",
-      ia: ""
+      ia: "",
     },
     {
       id: 5,
       title: "인프런 1분코딩<br />클론코딩, 3D 리플릿",
-      img:"/db/skills/s_slide_5.png",
+      img: "/db/skills/s_slide_5.png",
       type: ["적응형", "PC"],
       desc: "인터렉티브 웹 만들기 클론코딩",
       url: "https://leh317.iwinv.net/skills/slide/5",
-      ia: ""
+      ia: "",
     },
     {
       id: 6,
       title: "인프런 클론코딩<br />이미지를 활용한 패럴렉스 구현",
-      img:"/db/skills/s_slide_6.png",
+      img: "/db/skills/s_slide_6.png",
       type: ["적응형", "PC"],
       desc: "패럴렉스 구현 클론코딩",
       url: "https://leh317.iwinv.net/skills/slide/6",
-      ia: ""
-    }
+      ia: "",
+    },
   ],
   list: [
     {
       id: 1,
       title: "미러볼 회전 & 글리치 효과",
-      url: "https://leh317.iwinv.net/skills/list/1"
+      url: "https://leh317.iwinv.net/skills/list/1",
     },
     {
       id: 2,
       title: "라인따라 불빛이동 애니메이션",
-      url: "https://leh317.iwinv.net/skills/list/2"
+      url: "https://leh317.iwinv.net/skills/list/2",
     },
     {
       id: 3,
       title: "물방울 결합 배경",
-      url: "https://leh317.iwinv.net/skills/list/3"
+      url: "https://leh317.iwinv.net/skills/list/3",
     },
     {
       id: 4,
       title: "원형 & 배경화면 교체 효과",
-      url: "https://leh317.iwinv.net/skills/list/4"
+      url: "https://leh317.iwinv.net/skills/list/4",
     },
     {
       id: 5,
       title: "텍스트 롱 쉐도우 효과",
-      url: "https://leh317.iwinv.net/skills/list/5"
+      url: "https://leh317.iwinv.net/skills/list/5",
     },
     {
       id: 6,
       title: "Spotlight 효과",
-      url: "https://leh317.iwinv.net/skills/list/6"
+      url: "https://leh317.iwinv.net/skills/list/6",
     },
     {
       id: 7,
       title: "배너, 원형 점선 루프 회전",
-      url: "https://leh317.iwinv.net/skills/list/7"
+      url: "https://leh317.iwinv.net/skills/list/7",
     },
     {
       id: 8,
       title: "컬러풀한 배경 루프",
-      url: "https://leh317.iwinv.net/skills/list/8"
+      url: "https://leh317.iwinv.net/skills/list/8",
     },
     {
       id: 9,
       title: "텍스트 아웃라인 모션",
-      url: "https://leh317.iwinv.net/skills/list/9"
+      url: "https://leh317.iwinv.net/skills/list/9",
     },
     {
       id: 10,
       title: "손글씨 모션",
-      url: "https://leh317.iwinv.net/skills/list/10"
+      url: "https://leh317.iwinv.net/skills/list/10",
     },
     {
       id: 11,
       title: "폰트 객체별 확대 축소",
-      url: "https://leh317.iwinv.net/skills/list/11"
+      url: "https://leh317.iwinv.net/skills/list/11",
     },
     {
       id: 12,
       title: "떨어지는 낙엽 모션",
-      url: "https://leh317.iwinv.net/skills/list/12"
+      url: "https://leh317.iwinv.net/skills/list/12",
     },
     {
       id: 13,
       title: "마우스 오버, 텍스트 출력",
-      url: "https://leh317.iwinv.net/skills/list/13"
+      url: "https://leh317.iwinv.net/skills/list/13",
     },
     {
       id: 14,
       title: "마우스 오버, 움직이는 영역",
-      url: "https://leh317.iwinv.net/skills/list/14"
+      url: "https://leh317.iwinv.net/skills/list/14",
     },
     {
       id: 15,
       title: "체크박스 커스텀",
-      url: "https://leh317.iwinv.net/skills/list/15"
+      url: "https://leh317.iwinv.net/skills/list/15",
     },
     {
       id: 16,
       title: "마우스 오버, 회전 효과",
-      url: "https://leh317.iwinv.net/skills/list/16"
+      url: "https://leh317.iwinv.net/skills/list/16",
     },
     {
       id: 17,
       title: "전송 버튼 모션",
-      url: "https://leh317.iwinv.net/skills/list/17"
+      url: "https://leh317.iwinv.net/skills/list/17",
     },
     {
       id: 18,
       title: "장바구니 버튼 모션",
-      url: "https://leh317.iwinv.net/skills/list/18"
+      url: "https://leh317.iwinv.net/skills/list/18",
     },
     {
       id: 19,
       title: "햄버거 메뉴 버튼, 클릭 모션",
-      url: "https://leh317.iwinv.net/skills/list/19"
+      url: "https://leh317.iwinv.net/skills/list/19",
     },
     {
       id: 20,
       title: "Hotspot 애니메이션",
-      url: "https://leh317.iwinv.net/skills/list/20"
+      url: "https://leh317.iwinv.net/skills/list/20",
     },
     {
       id: 21,
       title: "마우스 커서 커스텀",
-      url: "https://leh317.iwinv.net/skills/list/21"
+      url: "https://leh317.iwinv.net/skills/list/21",
     },
     {
       id: 22,
       title: "마우스 오버, 차오르는 효과",
-      url: "https://leh317.iwinv.net/skills/list/22"
+      url: "https://leh317.iwinv.net/skills/list/22",
     },
     {
       id: 23,
       title: "패럴렉스 스크롤 이벤트",
-      url: "https://leh317.iwinv.net/skills/list/23"
+      url: "https://leh317.iwinv.net/skills/list/23",
     },
     {
       id: 24,
       title: "날아오는 글씨 효과",
-      url: "https://leh317.iwinv.net/skills/list/24"
+      url: "https://leh317.iwinv.net/skills/list/24",
     },
     {
       id: 25,
       title: "마우스 커서 커스텀(UI 구현)",
-      url: "https://leh317.iwinv.net/skills/list/25"
+      url: "https://leh317.iwinv.net/skills/list/25",
     },
     {
       id: 26,
       title: "문서 높이에 따른 가로 진행바",
-      url: "https://leh317.iwinv.net/skills/list/26"
+      url: "https://leh317.iwinv.net/skills/list/26",
     },
     {
       id: 27,
       title: "문서 높이에 따른 세로 진행바",
-      url: "https://leh317.iwinv.net/skills/list/27"
+      url: "https://leh317.iwinv.net/skills/list/27",
     },
     {
       id: 28,
       title: "translate를 사용한 패럴렉스 구현",
-      url: "https://leh317.iwinv.net/skills/list/28"
+      url: "https://leh317.iwinv.net/skills/list/28",
     },
     {
       id: 29,
       title: "scale를 사용한 패럴렉스 구현",
-      url: "https://leh317.iwinv.net/skills/list/29"
-    }
-  ]
-}
+      url: "https://leh317.iwinv.net/skills/list/29",
+    },
+  ],
+};
 
 export default DataSkills;
